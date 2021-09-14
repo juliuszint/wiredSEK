d64e2c463693c860a20d867447c3f16cb7204ae9
Julius Zint <zint.julius@gmail.com>
2021-09-14 15:24:54 +0200

    added small sample c program for testing the board

---
+++ hellomcu/main.c
@@ -0,0 +1,15 @@
+﻿#define F_CPU 1000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+	// Port E Pin 3 ist die Status LED
+	DDRE |= ( 1 << PE3); // configure pin as output
+	while (1) {
+		PORTE ^= (1 << PE3);
+		_delay_ms(500);
+	}
+	return 0;
+}
